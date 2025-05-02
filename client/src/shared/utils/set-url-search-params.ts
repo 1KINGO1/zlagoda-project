0dd261260f5457dd19ccc154bf31b38014188f52
KINGO <spental12345@gmail.com>
2025-05-02 20:33:53 +0300

    Fixed: param value might be not string and then .trim() cause error

--- client/src/shared/utils/set-url-search-params.ts
+++ client/src/shared/utils/set-url-search-params.ts
@@ -1,7 +1,10 @@
-export const setUrlSearchParams = (url: URL, params: object) => {
+export const setUrlSearchParams = (url: URL, params: Record<string, unknown>) => {
   for (const [key, value] of Object.entries(params)) {
-    if (value !== undefined && value.trim() !== '' && typeof value !== 'function') {
-      url.searchParams.set(key, value.toString())
-    }
+    if (
+      value === undefined ||
+      typeof value === 'function' ||
+      (typeof value === 'string' && value.trim() === '')
+    ) continue;
+    url.searchParams.set(key, String(value))
   }
 }
